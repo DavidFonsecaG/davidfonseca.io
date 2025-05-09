a5b79cb04e1d47e969fd81ff51428b7a8b858032
DavidFonsecaG <DavidFonsecaG@users.noreply.github.com>
2025-05-09 22:32:43 +0000

    deploy: 7702de40e0ac715cf17bfa1753015f1e998f49d9

--- _app/immutable/entry/start.B1dge_wR.js
+++
@@ -1,1 +0,0 @@
-import{l as o,s as r}from"../chunks/C_y8e2Oy.js";export{o as load_css,r as start};
---
+++ _app/immutable/entry/start.CGiJZaEh.js
@@ -0,0 +1,1 @@
+import{l as o,s as r}from"../chunks/Bfd6IcEh.js";export{o as load_css,r as start};
